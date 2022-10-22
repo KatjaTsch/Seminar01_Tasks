5caaf739336a18044108368ff16e1b02ba809f92
KatjaTsch <katjatschebotar.er@mail.ru>
2022-10-22 13:23:37 +0300

    Task_022

---
+++ Task_022/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 22: Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+
+String GetPows (int N)
+{
+    string result = "";
+    for (int i=1; i<=N-1; i++)
+    {
+        result += (i*i).ToString()+", ";
+    }
+    result += (N*N).ToString();
+    return result;
+}
+
+Console.Write("Введите N ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetPows(n));
