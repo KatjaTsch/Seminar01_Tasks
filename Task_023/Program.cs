5976ebb7a0c07086d0140765430bd15cd985637e
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-11 23:37:17 +0300

    added Task_023

---
+++ Task_023/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23: Напишите программу, которая принимает на
+//вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 4, 9.
+//5 -> 1, 8, 27, 64, 125
+
+String GetPows (int N)
+{
+    string result = "";
+    for (int i=1; i<=N-1; i++)
+    {
+        result += (i*i*i).ToString()+", ";
+    }
+    result += (N*N*N).ToString();
+    return result;
+}
+
+Console.Write("Введите число N ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetPows(n));
