e6c1549bb26a471067c32f0302c98d04211a77de
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-12 11:20:55 +0300

    added Task_028

---
+++ Task_028/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+int GetPro(int N)
+{
+    int Pro = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        Pro = Pro*i;
+        Console.WriteLine($"{i}: {Pro}");
+    }
+    return Pro;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetPro(A));
