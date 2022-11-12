4d27cadaf41c9aa998cf81eab0009d69ac3329cd
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-12 18:54:05 +0300

    Task_027

---
+++ Task_027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая
+//принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetSum(int N)
+{
+    int sum = 0;
+    while(N != 0)
+    {
+        sum = sum + N % 10;
+        N = N / 10;
+    }    
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма числа {A} равна {GetSum(A)}");
