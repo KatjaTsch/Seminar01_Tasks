d4508066b2835e83af278eee9b53af07a0641015
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-12 15:06:14 +0300

    added Task_035

---
+++ Task_035/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную
+//степень B. Реализовать функцию возведения в степень
+//самостоятельно!
+
+int Stepen(int A, int B)
+{
+   int pow = 1;
+   {
+      for(int i = 0; i < B; i++)
+      {
+         pow *= A;
+      }
+      return pow;
+   }  
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень числа");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{a} в степени {b} = {Stepen(a, b)}");
+
+
