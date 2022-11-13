383f94c53cc7ce48f76b776e0f87c4ee38b73319
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-13 10:47:28 +0300

    Task_029

---
+++ Task_029/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8
+//элементов и выводит их на экран.
+
+int[] FillArray()
+{
+    int[] a = new int[8];
+    
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        a[i] = int.Parse(Console.ReadLine());
+    }
+    return a;
+}
+Console.Clear();
+
+int[] array = FillArray();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
