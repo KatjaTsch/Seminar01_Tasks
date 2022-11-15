c354ec2fb684459cb9554bea8f84d586fa7f06e8
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-15 22:01:49 +0300

    Task_037

---
+++ Task_037/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент, второй и предпоследний
+//и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] doubleNum(int[] array)
+{
+    int size = array.Length / 2;
+
+    if (array.Length % 2 != 0)
+    {
+        size++;
+    }
+    int[] result = new int[size];
+
+    for (int i = 0; i < (array.Length / 2); i++)
+    {
+        result[i] = array[i] * array[array.Length - i - 1];
+    }
+
+    if (array.Length % 2 != 0)
+    {
+        result[size - 1] = array[array.Length / 2];
+    }
+
+    return result;
+
+}
+int[] array = GetArray(5, -5, 5);
+Console.WriteLine(String.Join(",", array));
+int[] array2 = doubleNum(array);
+Console.WriteLine(String.Join(",", array2));
