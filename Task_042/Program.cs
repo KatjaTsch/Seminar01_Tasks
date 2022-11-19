eebad0a60ff56f2ebb8bad259f0dcf67df6347ab
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-19 14:06:08 +0300

    Task_042

---
+++ Task_042/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+//десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+int GetBinariNumber(int number)
+{
+String result = "";
+if (number == 0) return 0;
+while (number > 0)
+{
+result = (number % 2).ToString() + result;
+number /= 2;
+}
+return int.Parse(result);
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(GetBinariNumber(num));
