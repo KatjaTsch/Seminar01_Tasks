8b1ecc801751b43889d966e0eb210cf808215ec3
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-20 17:43:29 +0300

    Task_.41

---
+++ Task_041/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] FillArray()
+{
+    int[] a = new int[M];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        a[i] = int.Parse(Console.ReadLine()!);
+    }
+    return a;
+}
+Console.Clear();
+
+int[] array = FillArray();
+for (int i = 0; i < array.Length; i++)
+{
+    int count = 0;
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                count = count + array[i];
+            }
+            return count;
+        }
+    }
+}
+
+int Num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(String.Join(",", FillArray(Num)));
+
+
+//int Function(int M)
+//{
+//  int[] array = new int[M];
+//int count = 0;
+//{
+//  for (int i = 0; i < array.Length; i++)
+//{
+//  if (array[i] > 0)
+//{
+//  count = count + array[i];
+//}
+//return count;
+//  }
+//}
+//}
+
+
+
+
