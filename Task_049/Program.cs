0f7ecdca6f1f4bdd4589fe1cb4c40d537d5c6cdd
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-23 19:21:25 +0300

    Task_049

---
+++ Task_049/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+//которых оба индекса чётные, и замените эти элементы на их
+//квадраты.
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SquareArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j += 2)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                inArray[i, j] *= inArray[i, j];
+            }
+        }
+        Console.WriteLine();
+    }
+    return inArray;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+
+PrintArray(SquareArray(array));
+
+
+
