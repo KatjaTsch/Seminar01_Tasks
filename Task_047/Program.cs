644993849c0e56eb803b973e3050ea824c7ccdd1
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-23 19:59:16 +0300

    Task_047

---
+++ Task_047/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+//заполненный случайными вещественными числами.
+
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * 10; //new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
