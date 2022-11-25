ebe461535a8c3a656badb558c1f0048a2a4b191d
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-25 23:44:05 +0300

    Task_052

---
+++ Task_052/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+//Найдите среднее арифметическое элементов в каждом
+//столбце.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetSum(int[,] Array1)
+{
+    int sum = 0;
+    for(int i = 0; i < Array1.GetLength(0); i++)
+    {
+        for(int j = 0; j < Array1.GetLength(1); j++)
+        {
+            sum = (sum + (Array1[i, j]))/j;
+        }
+        Console.WriteLine();
+    }
+    return Array1;
+}
+
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+
+int[,] array2 = GetSum(array);
+
