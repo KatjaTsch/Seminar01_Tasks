19603ce46da9723aece2f84e07064fb44f6763c0
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-29 19:19:29 +0300

    Task_054

---
+++ Task_054/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+//упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] Convert1d(int[,] array)
+{
+    int[] newarray = new int[array.GetLength(0) * array.GetLength(1)];
+    int index = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newarray[index++] = array[i, j];
+        }
+    }
+    return newarray;
+}
+
+void BoobleSort(int[] array)
+{
+    int temp;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[i] < array[j])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    Console.WriteLine(String.Join(", ", array) );
+}
+
+
+
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+int[] newarray = Convert1d(array);
+Console.WriteLine(String.Join(", ", newarray));
+Console.WriteLine();
+
+BoobleSort(newarray);
+
+
+
