4eeed87e5a8be4a54ae6387cd72c09507dd0f200
KatjaTsch <katjatschebotar.er@mail.ru>
2022-11-30 13:23:00 +0300

    Task_058

--- Task_058/Program.cs
+++ Task_058/Program.cs
@@ -7,41 +7,60 @@
 //18 20
 //15 18
 
-int[,] a = new int[2, 2];
-int[,] b = new int[2, 2];
-int[,] c = new int[2, 2];
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
 
-for (int i = 0; i < a.GetLength(0); i++)
-{
-    for (int j = 0; j < a.GetLength(1); j++)
+    for (int i = 0; i < m; i++)
     {
-        a[i, j] = new Random().Next(1, 2);
-        Console.Write("{0} ", a[i, j]);
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-for (int i = 0; i < a.GetLength(0); i++)
-{
-    for (int j = 0; j < a.GetLength(1); j++)
-    {
-        b[i, j] = new Random().Next(1, 3);
-        Console.Write("{0} ", b[i, j]);
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-
-for (int i = 0; i < c.GetLength(0); i++)
-{
-    for (int k = 0; k < c.GetLength(1); k++)
-    {
-        for (int j = 0; j < a.GetLength(1); j++)
+        for (int j = 0; j < n; j++)
         {
-            c[i, k] += a[j, k] * b[i, j];
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
-        Console.Write("{0} ", c[i, k]);
     }
-    Console.WriteLine();
+    return result;
 }
-Console.ReadLine();
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MatrixPow(int[,] array, int[,] newarray)
+{
+    int[,] result = new int[array.GetLength(0), newarray.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < newarray.GetLength(1); k++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                result[i, k] += newarray[j, k] * array[i, j];
+            }
+            Console.Write("{0} ", result[i, k]);
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
+
+
+
+int[,] array = GetArray(2, 2, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+int[,] array2 = GetArray(2, 2, 0, 10);
+PrintArray(array2);
+Console.WriteLine();
+
+int[,] array3 = MatrixPow(array, array2);
+
+
