6e332e4516b00636e6912b967a996240026e4c7c
KatjaTsch <katjatschebotar.er@mail.ru>
2022-12-03 13:30:50 +0300

    Task_066

---
+++ Task_066/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int SumDigits(int N, int M)
+{
+    int a = M;
+    int b = N;
+    if (M > N)
+    {
+       a = N;
+       b = M; 
+    } 
+    return (a + b)*(b - a + 1)/2;
+}
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!); 
+
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine()!); 
+
+Console.WriteLine(SumDigits(a, b));
