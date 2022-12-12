305d4115673745ca55020601c306996bc5c60cb6
KatjaTsch <katjatschebotar.er@mail.ru>
2022-12-12 22:22:38 +0300

    created the method

--- Final_Arbeit/Program.cs
+++ Final_Arbeit/Program.cs
@@ -13,5 +13,45 @@ string[] FillArray(int size)
     }
     return a;
 }
-
+Console.WriteLine();
 Console.Clear();
+
+string[] LengthOfElements(string[] array2)
+{
+    int length = array2.Length;			
+	string[] result = new string[length];
+	int count = 0;
+    
+    for (int i = 0; i < length; i++)
+    {
+        if (array2[i].Length <= 3)
+        {
+            result[count] = array2[i];
+            count++;
+        }
+    }
+    return result;
+    
+}
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write($"{inArray[i]} \t");
+    }
+    Console.WriteLine();
+}
+
+
+string[] array = FillArray(4);
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+string[] pos = LengthOfElements(array);
+Console.WriteLine();
+PrintArray(pos);
