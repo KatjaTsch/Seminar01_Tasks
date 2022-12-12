dbd8217319ba0afcfbb8eb4c92f56799dbf94ffd
KatjaTsch <katjatschebotar.er@mail.ru>
2022-12-12 19:19:40 +0300

    added method of elements

---
+++ Task_KA/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая из имеющегося массива строк сформирует массив строк, 
+//длина которых меньше либо равна 3 символа. Первонач. массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма.
+
+int[] FillArray(int size, int minValue, int maxValue)
+{
+    int[] a = new int[4];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return a;
+}
+Console.Clear();
+
+int[] array = FillArray(4, 100, 9999);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
